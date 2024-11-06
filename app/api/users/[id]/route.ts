82958997dde89c2ff2b35dc5656ff838474d4ef4
Aaron Lee <45613337+Fungeey@users.noreply.github.com>
2024-11-06 00:23:48 -0500

    formatting

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -6,16 +6,20 @@ interface userQuery{
   id?: string;
 }
 
+// Update user (by id)
 export async function PUT(request: NextRequest, query: userQuery){
   const {id} = query;
   const {newName : name, newRole: role } = await request.json();
+
   await connectMongoDB();
   await User.findByIdAndUpdate(id, {name, role});
   return NextResponse.json({message: "User updated"}, {status: 200});
 }
 
+// Read user (by id)
 export async function GET(request: NextRequest, query: userQuery){
   const {id} = query;
+
   await connectMongoDB();
   const user = await User.findOne({_id:id});
   return NextResponse.json({user}, {status:200});
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,22 +2,26 @@ import connectMongoDB from "@/lib/mongodb";
 import User from "@/models/user";
 import { NextResponse, NextRequest } from 'next/server';
 
+// Create new User
 export async function POST(request: NextRequest){
-    const {title, description} = await request.json();
-    await connectMongoDB();
-    await User.create(title, description);
+    const {name, role} = await request.json();
 
+    await connectMongoDB();
+    await User.create(name, role);
     return NextResponse.json({message: "User Created"}, {status:201});
 }
 
+// Read all users
 export async function GET() {
     await connectMongoDB();
     const users = await User.find();
     return NextResponse.json({ users });
 }
 
+// Delete user
 export async function DELETE(request: NextRequest){
     const id = request.nextUrl.searchParams.get("id");
+    
     await connectMongoDB();
     await User.findByIdAndDelete(id);
     return NextResponse.json({message: "User Deleted"}, {status: 200});
