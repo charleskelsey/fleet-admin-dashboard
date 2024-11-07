8fc14c6304b57efad24bc36e5bbae3c7667e7bdc
Saadia Shahid <saadia.shahid@torontomu.ca>
2024-11-07 13:41:07 -0500

    update style

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <main className="flex flex-col gap-8 w-full h-full p-56">
+      <main className="flex flex-col gap-6 w-full h-full pt-40 pb-4 px-12 border border-black">
         <h1 className="font-extrabold text-8xl flex text-left">
           ADMIN DASHBOARD
         </h1>
-        <div className="grid gap-2 grid-cols-2 grid-rows-2 h-1/2 w-full">
+        <div className="grid gap-2 grid-cols-2 grid-rows-2 h-full w-full border border-cyan-400 p-4">
           <Link href="/user-management" className="bg-cyan-400 text-white px-4 py-2 flex items-center justify-center w-full hover:bg-cyan-600">User Management</Link>
           <Link href="/rewards-management" className="bg-cyan-400 text-white px-4 py-2 flex items-center justify-center hover:bg-cyan-600">Rewards Management</Link>
           <Link href="/content-management" className="bg-cyan-400 text-white px-4 py-2 flex items-center justify-center hover:bg-cyan-600">Content Management</Link>
--- app/user-management/page.tsx
+++ app/user-management/page.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const userManagement = () => {
     return (
-        <div>userManagement</div>
+        <div className="h-screen flex items-center justify-center">
+            <main className="flex flex-col gap-6 w-full h-full pt-40 pb-4 px-12 border border-black">
+                <h1 className="font-extrabold text-8xl flex text-left">
+                    USER MANAGEMENT
+                </h1>
+                <div className="grid gap-2 grid-cols-2 grid-rows-2 h-full w-full border border-cyan-400 p-4">
+
+                </div>
+
+
+
+            </main>
+
+        </div>
     )
 }
 
