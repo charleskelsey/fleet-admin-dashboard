7d5261534fdc455c677d517d81f563c7aa0daac0
Saadia Shahid <saadia.shahid@torontomu.ca>
2024-11-07 12:00:22 -0500

    update homepage UI

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,17 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <main className="flex flex-col gap-8 items-center justify-center">
-        <h1 className="font-extrabold text-2xl">
+      <main className="flex flex-col gap-8 w-full h-full p-56">
+        <h1 className="font-extrabold text-8xl flex text-left">
           ADMIN DASHBOARD
         </h1>
+        <div className="grid gap-2 grid-cols-2 grid-rows-2 h-1/2 w-full">
+          <Link href="/user-management" className="bg-cyan-400 text-white px-4 py-2 flex items-center justify-center w-full hover:bg-cyan-600">User Management</Link>
+          <Link href="/rewards-management" className="bg-cyan-400 text-white px-4 py-2 flex items-center justify-center hover:bg-cyan-600">Rewards Management</Link>
+          <Link href="/content-management" className="bg-cyan-400 text-white px-4 py-2 flex items-center justify-center hover:bg-cyan-600">Content Management</Link>
+          <Link href="/ticket-management" className="bg-cyan-400 text-white px-4 py-2 flex items-center justify-center hover:bg-cyan-600">Ticket Management</Link>
+        </div>
 
-        <Link href="/user-management" className="bg-cyan-400 text-white px-4 py-2 rounded-full hover:bg-cyan-600">User Management</Link>
-        <Link href="/rewards-management" className="bg-cyan-400 text-white px-4 py-2 rounded-full  hover:bg-cyan-600">Rewards Management</Link>
-        <Link href="/content-management" className="bg-cyan-400 text-white px-4 py-2 rounded-full  hover:bg-cyan-600">Content Management</Link>
-        <Link href="/ticket-management" className="bg-cyan-400 text-white px-4 py-2 rounded-full  hover:bg-cyan-600">Ticket Management</Link>
 
 
       </main>
